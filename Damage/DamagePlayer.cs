f9cc2642c61227053cd9c40c748b5e9f7cd4c44a
luisJavierLuna <javierlunaluis@gmail.com>
2022-04-08 22:29:42 -0500

    Added the method of damaging the player.

--- Damage/DamagePlayer.cs
+++ Damage/DamagePlayer.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class DamagePlayer : MonoBehaviour
 {
+    [SerializeField] int damage;
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject
+                .GetComponent<HealthManager>()
+                .DamageCharacter(damage);
+        }
+    }
 }
