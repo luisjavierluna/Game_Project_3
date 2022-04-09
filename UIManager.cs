947dfcedcde818ec71470d7f2a5b1fbca5939e3f
luisJavierLuna <javierlunaluis@gmail.com>
2022-04-08 23:13:20 -0500

    User interface for player health created

--- UIManager.cs
+++ UIManager.cs
@@ -1,8 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Text;
 
 public class UIManager : MonoBehaviour
 {
+    [SerializeField] Slider barHP;
+    [SerializeField] Text textHP;
+    [SerializeField] HealthManager managerHP;
 
+    private void Update()
+    {
+        barHP.maxValue = managerHP.maxHealth;
+        barHP.value = managerHP.currentHealth;
+        StringBuilder sb = new StringBuilder("HP: ");
+        sb.Append(managerHP.currentHealth);
+        sb.Append("/");
+        sb.Append(managerHP.maxHealth);
+        textHP.text = sb.ToString();
+    }
 }
