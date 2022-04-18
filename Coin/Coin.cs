9bcb610445a06a64fd3f521901e21829a2c2fb0c
luisJavierLuna <javierlunaluis@gmail.com>
2022-04-17 21:50:18 -0500

    Added function to collect coins

--- Coin/Coin.cs
+++ Coin/Coin.cs
@@ -4,5 +4,21 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    [SerializeField] int value = 1;
 
+    CoinManager manager;
+
+    private void Start()
+    {
+        manager = FindObjectOfType<CoinManager>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            manager.AddCoin(value);
+            Destroy(gameObject);
+        }
+    }
 }
--- Coin/CoinManager.cs
+++ Coin/CoinManager.cs
@@ -1,8 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CoinManager : MonoBehaviour
 {
+    [SerializeField] Text coinText;
+    [SerializeField] int coin;
 
+    private void Start()
+    {
+        coinText.text = coin.ToString();
+    }
+
+    public void AddCoin(int newCoin)
+    {
+        coin += newCoin;
+        coinText.text = coin.ToString();
+    }
 }
